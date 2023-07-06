a310f1c1481639e54230c1aeafe7a19bdd1ebd72
Alkeops <m_adan2012@outlook.com>
2023-07-06 18:19:45 -0500

    Count

--- src/clases/Clase04/Clase04.jsx
+++ src/clases/Clase04/Clase04.jsx
@@ -1,3 +1,4 @@
+import { Counter } from "../../components/Counter";
 import { Button, Title, FlexComponent, Input } from "../../components/common";
 import {useState} from "react";
 /* 
@@ -9,7 +10,6 @@ Button puede recibir variante primary o secondary
 
 ctrl + p y nombre para abrir el archivo directamente
 */
-
 export const Clase04 = () => {
   const [value, setValue] = useState("");
   return (
@@ -22,6 +22,11 @@ export const Clase04 = () => {
         <Button label="Boton sin variant" />
         <Button label="Boton secondary" variant="secondary" />
         <Input placeholder="Un input simple" value={value} onChange={({target}) => setValue(target.value)} />
+      </FlexComponent>
+      <Title label="Componentes con estado" variant="subtitle" />
+      {/* Componentes con estado */}
+      <FlexComponent direction="row" gap="24px">
+        <Counter stock={10} onAdd={(qty)=>console.log(`van ${qty}`)} />
       </FlexComponent>
     </FlexComponent>
   );
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { Button, FlexComponent } from "../common";
+
+export const Counter = ({ stock, onAdd }) => {
+  const [count, setCount] = useState(1);
+
+  return (
+    <FlexComponent gap="16px">
+      <FlexComponent direction="row" gap="16px">
+        <Button label="-" onClick={() => setCount(Math.max(1, count - 1))} />
+        <span>{count}</span>
+        <Button
+          label="+"
+          onClick={() => setCount(Math.min(stock, count + 1))}
+        />
+      </FlexComponent>
+      <Button
+        label="Agregar al carrito"
+        variant="secondary"
+        onClick={() => onAdd(count)}
+        disabled={!stock}
+      />
+    </FlexComponent>
+  );
+};
