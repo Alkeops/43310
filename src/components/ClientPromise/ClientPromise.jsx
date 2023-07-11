f15d59e9b5b65079d647b401503fd4080fa1da2a
Alkeops <m_adan2012@outlook.com>
2023-07-10 21:28:45 -0500

    Comments

--- src/components/ClientPromise/ClientPromise.jsx
+++ src/components/ClientPromise/ClientPromise.jsx
@@ -1,13 +1,44 @@
+import { useEffect, useState } from "react";
 import { FlexComponent, Title } from "../common";
 
+const prometoQueVoy = () =>
+  new Promise((res, rej) => {
+
+    const siva = Math.random() <= 0.5;
+
+    setTimeout(() => {
+
+      if(siva) res("Vino")
+      rej("Desocupo las mesas");
+
+    }, 3500);
+  });
+
+
 export const ClientPromise = () => {
+  const [isLoading, setIsLoading] = useState(false); //Un estado para cargando
+  const [hasError, setHasError] = useState(null); //Un estado para error
+  const [llega, setLlega] = useState(null); //Un estado para el 'resultado'
+
+  useEffect(()=>{
+    //Al comienzo (cuando se monta) se incializa el isLoading en true
+    setIsLoading(true);
+    prometoQueVoy()
+      .then(res => { setLlega(res)} ) //Si se cumple
+      .catch(error => setHasError(error)) //Si falla
+      .finally(() => setIsLoading(false)) //De cualquiera de las dos maneras
+      
+  },[])
+
   return (
     <FlexComponent align={"flex-start"} gap="40px">
-      <Title label=""/* Prometo ir */ variant="subtitle" />
+      <Title label="Prometo ir" variant="subtitle" />
 
       <FlexComponent direction={"row"} gap="24px">
+        <Title label={isLoading ? "Cargando.." :  "Cargo "} />
+        <Title label={hasError ? hasError : "Sin error"} />
         {/* ¿Vendra? */}
-        <Title label={""} variant="subtitle" />
+        <Title label={llega} variant="subtitle" />
       </FlexComponent>
     </FlexComponent>
   );
--- src/components/PaymentPromise/PaymentPromise.jsx
+++ src/components/PaymentPromise/PaymentPromise.jsx
@@ -1,14 +1,62 @@
 import { useEffect, useState } from "react";
 import { Button, FlexComponent, Title } from "../common";
 
+
+//Funcion que retorna una promesa dependiendo de un valor aleatorio (true o false)
+const tePrometoQueTePago = () =>
+  new Promise((res, rej) => {
+    const paga = Math.random() <= 0.5;
+
+    setTimeout(() => {
+      if (paga) res("Ten tu dinero"); //Si es true lo resuelve
+
+      rej("No, Tu tienes mucho"); //Si no lo cancela
+    }, 1500);
+  });
+
 export const PaymentPromise = () => {
+  const [pagara, setPagara] = useState(null); //El estado inicializa en null
+
+  useEffect(() => {
+    /* ASYNC Y await */
+    //Se necesita crear una funcion asincrona dentro de el useEffect
+    const handleRequest = async () => {
+      try {
+        const data = await tePrometoQueTePago();
+        setPagara(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    if (!pagara) {
+      //Como queremos evitar bucles infinitos de lectura-seteo, tenemos una condicional solo sí es un valor falsy hace la "peticion"
+      handleRequest(); //Se llama a la funcion asincrona
+      /*
+      Manejar promesa 
+
+    tePrometoQueTePago()
+    .then((res) => {
+      setPagara(res);
+    })
+    .catch((err) => setPagara(err)); 
+    */
+    }
+  }, [pagara]); //Tiene como dependencia pagara
+
   return (
     <FlexComponent align={"flex-start"} gap="40px">
       <Title label="Prometo pagar" variant="subtitle" />
 
       <FlexComponent direction={"row"} gap="24px">
         {/* ¿Pagara? */}
-        <Title label={""} variant="subtitle" />
+        <Button
+          label="¿Me vas a pagar?"
+          onClick={() => setPagara(null)} 
+          //El boton pasa el estado a null lo que detona otra vez la peticion 
+          //(Esto podria estar en una funcion aparte y no depender del useEffect)
+        />
+        <Title label={pagara} variant="subtitle" />
       </FlexComponent>
     </FlexComponent>
   );
