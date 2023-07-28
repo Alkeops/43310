ec35c025ae42a886da5aa4b2c7c1604762818d5c
Alkeops <m_adan2012@outlook.com>
2023-07-27 19:27:58 -0500

    Req

--- src/components/Nasa/Nasa.jsx
+++ src/components/Nasa/Nasa.jsx
@@ -4,30 +4,9 @@ import { Button, FlexComponent, Title } from "../common";
 import { useQuery } from "@tanstack/react-query";
 import { useNasaData } from "../../lib/nasa.requests";
 
-const API_KEY = "gkNJiVBizkgyhGzb9ujNoi5Bu8D9L2szj29qN1nO";
-const API_URL = "https://api.nasa.gov";
-//PETICION A LA API DE LA NASA
-//Tiene una api_url que se envia como query param
-
-const handleRequest = async () => {
-  const response = await fetch(
-    `${API_URL}/planetary/apod?api_key=${API_KEY}&count=5`
-  ); //API_URL + ENDPOINT + QUERY PARAM
-  return await response.json();
-};
 
 export const Nasa = () => {
-  //Ejemplo mas simple
   const { data, isLoading, isError, isSuccess } = useNasaData();
-
-  //Un estado para manejar la data de la respuesta (y mostrarla al usuario)
-  //Un estado de loading
-  /*   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false); */
-
-  /* useEffect(() => {
-    handleRequest();
-  }, []); */
 
   return (
     <FlexComponent align="flex-start">
